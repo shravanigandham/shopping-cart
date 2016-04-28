96e1314acc06623693020037a3271f5da91547bc
shravanigandham <shravani.gandham470@gmail.com>
2016-04-28 09:56:19 +0530

    Update Cust_registration.java

--- Cust_registration.java
+++ Cust_registration.java
@@ -1,4 +1,4 @@
-package customer_registration;
+package shoppingmall;
 import java.io.*;
 
 public class Cust_registration {
@@ -6,10 +6,11 @@ String cust_name;
 long cust_ph_number;
 String cust_email_id;
 String type_of_customer;
+static int num_of_cust;
 BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 public void setCustomerDetails() throws IOException{
 	System.out.println("enter number of customers entering into the store : ");
-	int num_of_cust=Integer.parseInt(br.readLine()); 
+	num_of_cust=Integer.parseInt(br.readLine()); 
 	for(int i=1;i<=num_of_cust;i++){
 		System.out.println("enter customer "+i+" name,phone number,type");
 		cust_name=br.readLine();
